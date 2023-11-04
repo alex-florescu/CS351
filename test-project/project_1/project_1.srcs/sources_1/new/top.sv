e13e000491bd92ccd9124414cb5ed4bf69c4df5d
Alex Florescu <alex.florescu@warwick.ac.uk>
2023-11-04 09:40:44 +0000

    added test project
    a test project to confirm that i can use the onedrive folder

---
+++ test-project/project_1/project_1.srcs/sources_1/new/top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.11.2023 09:36:43
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+
+    );
+endmodule
